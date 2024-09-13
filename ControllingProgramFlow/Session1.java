b0cd2c87464865d04f73d06d67e98ea50d88cd94
Lutho Mangwana <luthomangwana@gmail.com>
2024-09-13 16:36:56 +0200

    13 September 2024 16:36

---
+++ ControllingProgramFlow/Session1.java
@@ -0,0 +1,32 @@
+package day11.ControllingProgramFlow;
+
+public class Session1 {
+	
+	public void q1() {}
+	public void q2() {}
+	public void q3() {}
+	public void q4() {}
+	public void q5() {}
+	public void q6() {}
+	public void q7() {}
+	public void q8() {}
+	public void q9() {}
+	public void q10() {}
+	public void q11() {}
+	public void q12() {}
+	public void q13() {}
+	public void q14() {}
+	public void q15() {}
+	public void q16() {}
+	public void q17() {}
+	public void q18() {}
+	public void q19() {}
+	public void q20() {}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Session1 obj = new Session1();
+		
+	}
+
+}
---
+++ ControllingProgramFlow/Session2.java
@@ -0,0 +1,32 @@
+package day11.ControllingProgramFlow;
+
+public class Session2 {
+
+	public void q21() {}
+	public void q22() {}
+	public void q23() {}
+	public void q24() {}
+	public void q25() {}
+	public void q26() {}
+	public void q27() {}
+	public void q28() {}
+	public void q29() {}
+	public void q30() {}
+	public void q31() {}
+	public void q32() {}
+	public void q33() {}
+	public void q34() {}
+	public void q35() {}
+	public void q36() {}
+	public void q37() {}
+	public void q38() {}
+	public void q39() {}
+	public void q40() {}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Session2 obj = new Session2();
+	}
+
+}
